ab34d217c4454b622f790bd8f2fce90c9ede9a02
Kalana Madhumalka <kalanam890@gmail.com>
2025-02-13 15:02:06 +0530

    Navbar first commit by Kalana, logo is in public as logo.png , change as you wish

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import Navbar from "@/app/Components/Navbar";
 
 export default function Home() {
   return (
+  <>
+  <Navbar />
    <main>
-    <h1>
-      Hello world
-    </h1>
+    <h1>Hello world</h1>
    </main>
+  </>
   );
 }
