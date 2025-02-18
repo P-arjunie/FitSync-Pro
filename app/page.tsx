ae9cf51a3a57bdad1ca364843d2f7031d1ab9d20
LithiraUL <158991306+LithiraUL@users.noreply.github.com>
2025-02-18 08:00:44 +0530

    2nd edition Authform, userinfo, memberregform

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+import AuthForm from "./lithira/Authform/page";  
+import UserManagement from "./lithira/userinfo/page";  // I assume this is being used later
+
+const ParentComponent:React.FC = () => {
+  const handleNewUser = (user: { name: string; role: "member" | "trainer" }) => {
+    console.log("New user created:", user);
+  };
+
   return (
-   <main>
-    <h1>
-      Hello world
-    </h1>
-   </main>
+    <div>
+      {/* Pass handleNewUser as onNewUser prop */}
+      <AuthForm onNewUser={handleNewUser} />
+    </div>
   );
-}
+};
+
+export default ParentComponent;
+
+
+
