74fc40a25dad03348a33ac8a48fefe193af7947a
Pasindi Arjunie <154939837+P-arjunie@users.noreply.github.com>
2025-05-14 16:19:02 +0530

    fix

--- app/pasindi/components/GoogleCalendarEvents.tsx
+++ app/pasindi/components/GoogleCalendarEvents.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useSession } from 'next-auth/react';
 import { Session } from 'next-auth';
